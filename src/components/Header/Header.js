36d87e7ad700fd8610691a027d5762a01fdcbfb2
wigillette <wgillette02@gmail.com>
2021-12-28 20:22:57 -0500

    Header Update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -40,11 +40,11 @@ const SideBar = () => {
         <Offcanvas.Body>
           <h4>Navigation</h4>
           <ul className="list-unstyled navigation-links">
-            <li><Link to = "/home">Home</Link></li>
-            <li><Link to = "/coursework">Course Work</Link></li>
-            <li><Link to = "/portfolio">Portfolio</Link></li>
-            <li><Link to = "/athletics">Athletics</Link></li>
-            <li><Link to = "/contact">Contact Me</Link></li>             
+            <li><Link to = "/home"><span className = "fa fa-home fa-lg"></span> Home</Link></li>
+            <li><Link to = "/coursework"><span className = "fa fa-graduation-cap fa-lg"></span> Course Work</Link></li>
+            <li><Link to = "/portfolio"><span className = "fa fa-folder-o fa-lg"></span> Portfolio</Link></li>
+            <li><Link to = "/athletics"><span className = "fa fa-line-chart fa-lg"></span> Athletics</Link></li>
+            <li><Link to = "/contact"><span className = "fa fa-id-card fa-lg"></span> Contact Me</Link></li>             
           </ul>
           <hr/>
           <Quote quote="Geniuses do not exist in the world." author = "Chris Tralie, Ph.D."></Quote>
@@ -61,11 +61,11 @@ const Header = () => (
         <Container>
           <Navbar.Brand className = "brand" href="/home"><h4 className = "header-link">William Gillette</h4></Navbar.Brand>
           <Nav>
-            <Nav.Link className="h-link" href = "/home"><h4 className = "header-link">Home</h4></Nav.Link>
-            <Nav.Link className="h-link" href = "/coursework"><h4 className = "header-link">Course Work</h4></Nav.Link>
-            <Nav.Link className="h-link" href = "/portfolio"><h4 className = "header-link">Portfolio</h4></Nav.Link>
-            <Nav.Link className="h-link" href = "/athletics"><h4 className = "header-link">Athletics</h4></Nav.Link>
-            <Nav.Link className="h-link" href = "/contact"><h4 className = "header-link">Contact Me</h4></Nav.Link> 
+            <Nav.Link className="h-link" href = "/home"><span className = "fa fa-home fa-lg"></span><h4 className = "header-link">Home</h4></Nav.Link>
+            <Nav.Link className="h-link" href = "/coursework"><span className = "fa fa-graduation-cap fa-lg"></span><h4 className = "header-link">Course Work</h4></Nav.Link>
+            <Nav.Link className="h-link" href = "/portfolio"><span className = "fa fa-folder-o fa-lg"></span><h4 className = "header-link">Portfolio</h4></Nav.Link>
+            <Nav.Link className="h-link" href = "/athletics"><span className = "fa fa-line-chart fa-lg"></span><h4 className = "header-link">Athletics</h4></Nav.Link>
+            <Nav.Link className="h-link" href = "/contact"><span className = "fa fa-id-card fa-lg"></span><h4 className = "header-link">Contact Me</h4></Nav.Link> 
           </Nav>
         </Container>
       </Navbar>
